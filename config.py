cab2f0d55f43a43c8448905e53a851a45a3c7016
arda0142 <151012820+arda0142@users.noreply.github.com>
2025-12-23 06:53:02 +0300

    Update and rename aCO to config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+
+# ACO Parametreleri
+KARINCA_SAYISI = 20
+ITERASYON = 100
+ALPHA = 1.0  # Feromon önemi
+BETA = 3.0   # Mesafe önemi
+RHO = 0.5    # Buharlaşma oranı
+Q = 100      # Feromon bırakma sabiti
+BASLANGIC_FEROMON = 0.1
